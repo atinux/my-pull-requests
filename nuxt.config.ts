6a300bb7c93f2a654b4d379af89470c32b4e4d7e
Sébastien Chopin <seb@nuxt.com>
2024-08-21 10:50:05 +0200

    move to app.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'en'
-      }
-    }
-  },
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
 
